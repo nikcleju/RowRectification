3cde8b62c6885b662c62a5e1691cf08192a43e54
Nicolae Cleju <nikcleju@gmail.com>
2022-09-28 20:43:07 +0300

    Update

--- Ideea1_FilterAndRidge.py
+++ Ideea1_FilterAndRidge.py
@@ -1,37 +1,40 @@
 # %%
-#import cv2
 import numpy as np
+import scipy as sp
+import scipy.interpolate
 import skimage as ski
 import skimage.color, skimage.filters
 
-import matplotlib
-matplotlib.use('Qt5Agg')  # non-interactive
 import matplotlib.pyplot as plt
 
 from utils import ridge_following
 
 I = plt.imread('Rows.png')
 I = ski.color.rgb2gray(I)
-# plt.imshow(I, cmap='gray')
+plt.figure()
+plt.imshow(I, cmap='gray')
 # plt.show()
 
 # Invert
 maxval = np.max(I)
 Iinv = maxval - I
-# plt.imshow(I, cmap='gray')
+plt.figure()
+plt.imshow(Iinv, cmap='gray')
 # plt.show()
 
 # Gaussian filtering
 If = ski.filters.gaussian(Iinv, sigma=[5, 50])
+plt.figure()
 plt.imshow(If, cmap='gray')
-plt.show()
+# plt.show()
 
 # Histogram of a band in the middle part
-mid_width = int(I.shape[1] / 2)
+band_mid = int(I.shape[1] / 2)
 band_halfwidth = 50
-h = np.sum(If[:,mid_width - band_halfwidth : mid_width + band_halfwidth], axis=1)
+h = np.sum(If[:, band_mid-band_halfwidth:band_mid+band_halfwidth], axis=1)
+plt.figure()
 plt.plot(h)
-plt.show()
+# plt.show()
 
 # Extract peaks
 # Must do it automatically
@@ -41,6 +44,7 @@ peaks = [40, 100, 150, 200, 250, 300, 352, 401, 450, 500]
 If2 = np.copy(I)
 
 # Follow ridges and plot
+ridges = []
 for peak in peaks:
 
     ridge = ridge_following(If, [peak,mid_width], stop_thresh=0.5)
@@ -50,6 +54,23 @@ for peak in peaks:
         if ridge[c] != -1:
             If2[ridge[c],c] = 0
 
+    ridges.append(ridge)
+
+plt.figure()
 plt.imshow(If2, cmap='gray')
-plt.show() 
+
+# Smoothing
+for ridge in ridges:
+
+    # Limit to part != -1
+    x = np.array(range(I.shape[1]))
+    x = x[ridge != -1]    
+    ridge = ridge[ridge != -1]
+
+    spl = sp.interpolate.UnivariateSpline(x, ridge, k=1, s=len(x)/2)
+    plt.figure()
+    plt.plot(ridge)
+    plt.plot(spl(x))
+    plt.gca().invert_yaxis()
+    plt.show() 
 
